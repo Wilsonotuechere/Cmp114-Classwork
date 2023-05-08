9087757952e286c2e1068bbd86f347b73c454803
Wilson <wilson.otuechere@binghamuni.edu.ng>
2023-05-08 15:35:35 +0100

    wilson otuechere's work

---
+++ Physics.py
@@ -0,0 +1,14 @@
+class Physics:
+    def __init__(self):
+        pass
+
+    def calc_average_speed(self, d, t):
+        return d/t
+    def calc_newtons_second_law(self, m, a):
+        return m*a
+    def calc_power(self, w, t):
+        return w/t
+    def calc_workdone(self, f, d):
+        return f*d
+    def calc_momentum(self, M, V):
+        return M*V
---
+++ maths.py
@@ -0,0 +1,23 @@
+class Math:
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return a / b
+
+    def square_root(self, a):
+        if a < 0:
+            raise ValueError("Cannot calculate square root of negative number")
+        return a ** 0.5
+
