9212757cff0f1680ebe07ea1194ba1ceb5c7faed
sesughcaleb3 <sesughwilliams@gmail.com>
2023-05-08 15:45:28 -0700

    sesugh's contribution

---
+++ caleb's physics file.py
@@ -0,0 +1,16 @@
+class Physics:
+
+    def calc_average_speed(self, d, t):
+        return d/t
+
+    def calc_newtons_second_law(self, m, a):
+        return m*a
+
+    def calc_power(self, w, t):
+        return w/t
+
+    def calc_workdone(self, f, d):
+        return f*d
+
+    def calc_momentum(self, M, V):
+        return M*V
